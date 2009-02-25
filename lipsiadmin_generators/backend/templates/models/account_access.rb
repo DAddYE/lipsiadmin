186472113ac7d46c4c33556457b3c210f3971329
Davide D'Agostino <DAddYE@DAddYE.local>
2009-02-25 17:38:58 +0100

    Fix a typo

--- lipsiadmin_generators/backend/templates/models/account_access.rb
+++ lipsiadmin_generators/backend/templates/models/account_access.rb
@@ -3,7 +3,6 @@ class AccountAccess < Lipsiadmin::AccessControl::Base
   roles_for :administrator do |role|
     # Shared Permission
     role.allow_all_actions "/backend/base"
-    role.allow_all_actions "/backend/event_logs"
     
     role.project_module :Account do |project|
       project.menu :list,   "/backend/accounts.js" do |submenu|
