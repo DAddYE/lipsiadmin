7bea1fe4b2f284d57128cacf300aef79c5f30154
Davide D'Agostino <DAddYE@DAddYE.local>
2009-02-27 16:08:52 +0100

    Fixed another issue with attachment table

--- lipsiadmin_generators/attachment/templates/migration.rb
+++ lipsiadmin_generators/attachment/templates/migration.rb
@@ -1,6 +1,6 @@
 class CreateAttachment < ActiveRecord::Migration
   def self.up
-    create_table :attachment, :force => true do |t|
+    create_table :attachments, :force => true do |t|
       t.string  :attached_file_name, :attached_content_type
       t.integer :attached_file_size
       t.timestamps
